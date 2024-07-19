f3f34eb70304067937a7fb31b9e2ec846d351c03
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-07-19 18:26:20 +0530

    minor changes

--- client/src/pages/Compiler.tsx
+++ client/src/pages/Compiler.tsx
@@ -9,17 +9,13 @@ import {
 } from "@/components/ui/resizable";
 import { useLoadCodeMutation } from "@/redux/slices/api";
 import { updateFullCode, updateIsOwner } from "@/redux/slices/compilerSlice";
-import { RootState } from "@/redux/store";
 import { handleError } from "@/utils/handleError";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 export default function Compiler() {
   const { urlId } = useParams();
-  const windowWidth = useSelector(
-    (state: RootState) => state.appSlice.currentWidth
-  );
   const [loadExistingCode, { isLoading }] = useLoadCodeMutation();
   const dispatch = useDispatch();
 
@@ -49,14 +45,10 @@ export default function Compiler() {
     );
   return (
     <ResizablePanelGroup
-      direction={windowWidth > 768 ? "horizontal" : "vertical"}
+      direction="horizontal"
       className="!h-[calc(100vh-60px)]"
     >
-      <ResizablePanel
-        // className="h-[calc(100dvh-60px)] min-w-[350px]"
-        defaultSize={50}
-        className="h-[500px]"
-      >
+      <ResizablePanel defaultSize={50} className="h-[500px]">
         <HelperHeader />
         <CodeEditor />
       </ResizablePanel>
--- client/src/redux/slices/appSlice.ts
+++ client/src/redux/slices/appSlice.ts
@@ -8,13 +8,11 @@ export interface appSliceState {
     savedCodes?: string[];
   };
   isLoggedIn: boolean;
-  currentWidth: number;
 }
 
 const initialState: appSliceState = {
   currentUser: {},
   isLoggedIn: false,
-  currentWidth: window.innerWidth,
 };
 
 const appSlice = createSlice({
@@ -30,11 +28,8 @@ const appSlice = createSlice({
     updateIsLoggedIn: (state, action: PayloadAction<boolean>) => {
       state.isLoggedIn = action.payload;
     },
-    setCurrentWidth: (state, action: PayloadAction<number>) => {
-      state.currentWidth = action.payload;
-    },
   },
 });
 
 export default appSlice.reducer;
-export const { updateCurrentUser, updateIsLoggedIn,setCurrentWidth } = appSlice.actions;
+export const { updateCurrentUser, updateIsLoggedIn } = appSlice.actions;
